275018974518bf7dbe47f6474357475203c83f4c
gregoryshoniwa <gregoryshoniwa@gmail.com>
2023-04-02 04:02:49 +0200

    added static use apps for css and js

--- index.js
+++ index.js
@@ -8018,8 +8018,10 @@ app.get('/getAdmin', async (req, res) => {
 
 
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/images', express.static(path.join(__dirname, 'public/img')))
+app.use('/css', express.static(path.join(__dirname, 'public/css')))
+app.use('/js', express.static(path.join(__dirname, 'public/js')))
 
 
 app.get("/", (req,res) =>{
